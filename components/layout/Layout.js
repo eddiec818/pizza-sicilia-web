99143d2849ee680cab0eb1864630af6db8a9e910
Leslie Cover <ecover18@gmail.com>
2020-07-09 19:35:34 -0500

    Hubo un error.

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
-import Header from "./Header";
 import AltHeader from "./AltHeader";
 import Footer from "./Footer";
 import CartSideBar from "./CartSideBar";
